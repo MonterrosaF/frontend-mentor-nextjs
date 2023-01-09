f95ba15fb921233ff9b2255dc90db25c913e62c6
Andres Felipe Monterrosa Alarcon <felipe.monterrosa@hotmail.com>
2023-01-08 23:46:07 -0500

    feat: adding new config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+    },
+    fontSize: {
+      'body-m': ['14px', '21px'],
+    },
+    fontFamily: {
+      regular: ['Manrope'],
+      medium: ['Manrope'],
+      bold: ['Manrope'],
+    },
+    fontWeight: {
+      regular: 300,
+      medium: 600,
+      bold: 800,
+    },
   },
   plugins: [],
 };
